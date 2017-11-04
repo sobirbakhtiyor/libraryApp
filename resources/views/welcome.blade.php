add95fa3c967ec95de6f0ab288c31deaae7bf86d
sobirbakhtiyor <sobirbakhtiyor@gmail.com>
2017-11-05 03:06:29 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">Welcome</div>
-                    nononononononononononononononon
+
                 
             </div>
         </div>
