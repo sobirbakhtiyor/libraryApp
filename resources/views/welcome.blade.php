52133c53dec2ad4258ae664c11ff8550da8d503e
Sarvar Alibekov <sarvar.bakhtiyor@gmail.com>
2017-11-04 19:18:04 +0500

    test 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Welcome</div>
 
                 <div class="panel-body">
-                    Welcome :p
+                    Welcome :p :)
                 </div>
             </div>
         </div>
