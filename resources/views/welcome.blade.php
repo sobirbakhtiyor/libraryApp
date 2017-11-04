c8cf57ca7904c2454acab422c2bc2ed3d1851f19
Sarvar Alibekov <sarvar.bakhtiyor@gmail.com>
2017-11-05 01:11:43 +0500

    test commit 4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Welcome</div>
 
                 <div class="panel-body">
-                    Welcome :p :)
+                   development testing
                 </div>
             </div>
         </div>
